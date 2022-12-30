57e36c0916a3e1915762293aaf35d2aa6e0d54e2
stas denysenko <denisenko924@gmail.com>
2022-12-30 15:19:49 +0200

    initial commit

---
+++ src/main/java/com/denisennko/mps/mpsdevelopmenttesttask/MpsDevelopmentTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.denisennko.mps.mpsdevelopmenttesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpsDevelopmentTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpsDevelopmentTestTaskApplication.class, args);
+	}
+
+}
